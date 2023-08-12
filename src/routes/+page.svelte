af3ac261f063b946c3ac5dee8c9e32462e328e82
Deepakraj <deepakatr4@gmail.com>
2023-08-12 19:23:13 +0530

    remote

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+
+    import { v4 as uuidv4 } from 'uuid'
+    import axios from 'axios'
+
+    let email = ''
+    /**
+     * @type {any}
+     */
+    let file
+
+    let uploadStatus = 'uploading....'
+
+    const onUpload = () => {
+        console.log(file)
+    }
+
+    const handleEmail = () => {
+        localStorage.setItem('email', email)
+    }
+
+</script>
+
+
+<div>
+    <input type="text" name="email" bind:value={email} />
+    <button on:click={() => handleEmail()}>Enter Email</button>
+    <h1>File upload status {uploadStatus}</h1>
+
+    <input type="file" name="file" bind:value={file} />
+    <button on:click={() => onUpload()}>Upload</button>
+</div>
