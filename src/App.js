e2180f6f4cb8211bd6b74074a0484c6d93c72053
Deepakraj <deepakatr4@gmail.com>
2023-08-14 14:29:14 +0530

    sample upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [email, setEmail] = useState('')
+  const [file, setFile] = useState('')
+  const [progress, setProgress] = useState(0)
+  const [isCompleted, setIsCompleted] = useState(false)
+  const formData = new FormData()
+
+  const handleOnUpload = async () => {
+    formData.append('email', email)
+    formData.append('name', file.name)
+    formData.append('format', file.name.split('.')[1])
+    formData.append('file', file)
+
+    const data = await axios.post('http://localhost:5002/api/v1/upload', formData, {
+      onUploadProgress: (progressEvent) => {
+        setProgress(Math.round(progressEvent.loaded / progressEvent.total * 100))
+      }
+    })
+    console.log(data)
+
+    if(data.status === 200) {
+      setIsCompleted(true)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <h2 className="title">Got Shared - Sharing made easy</h2>
+
+      <p>Progress: {progress}</p>
+      <p>{isCompleted ? 'Completed' : 'Uploading...'}</p>
+
+      <div>
+        <div>
+          Email: <input type="email" label="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <input type="file" onChange={e => setFile(e.target.files[0])} />
+          <button onClick={() => handleOnUpload()}>Upload</button>
+        </div>
+      </div>
     </div>
-  );
+    
+
+  )
+
 }
 
-export default App;
+export default App
